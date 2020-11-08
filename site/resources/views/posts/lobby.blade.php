2d4cb440885cbe91ba51c162cbaabde15493ea0d
Mikemaro <mikemaro1996@gmail.com>
2020-11-08 14:23:20 +0000

    Update lobby.blade.php

--- site/resources/views/posts/lobby.blade.php
+++ site/resources/views/posts/lobby.blade.php
@@ -5,7 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
           <div class="form-group row">
-            <label for="name" type="text" class="form-control{{$errors->has('name') ? 'is invalid'}}" name="name" value="{{old('name')}}" autocomplete>
+            <label for="name" class="col-md-4-form-label text-md-right">Name:</label>
+            <div class="col-md-6">
+              <input for="name" type="text" class="form-control{{$errors->has('name') ? 'is invalid' : ''}}" name="name" value="{{old('name')}}" autocomplete="name" autofocus>
+
+              @if ($errors->has('caption'))
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{$errors->first('name')}}</strong>
+                </span>
+              @endif
+
+            </div>
           </div>
         </div>
 @endsection
